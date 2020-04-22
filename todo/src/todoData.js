2a0bc777e28c14a6b0d8c469b214ac32693ebef8
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-22 14:28:14 -0400

    Created todoData.js
    The array of todos in todoData.js will be the values that will be used for initial state.

---
+++ todo/src/todoData.js
@@ -0,0 +1,24 @@
+const todoData = [
+	{
+		task: 'Clean the Apartment',
+		id: 1528817077286,
+		completed: false
+	},
+	{
+		task: 'Get Groceries',
+		id: 1528817084358,
+		completed: false
+	},
+	{
+		task: 'Wash Car',
+		id: 1528817084359,
+		completed: false
+	},
+	{
+		task: 'Update Resume',
+		id: 1528817084360,
+		completed: false
+	}
+];
+
+export default todoData;
