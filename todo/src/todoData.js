72c6490e04a289a58e30899ffe50e419c3570b92
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-05-11 21:51:45 -0400

    Update todoData.js

--- todo/src/todoData.js
+++ todo/src/todoData.js
@@ -1,23 +1,8 @@
 const todoData = [
 	{
-		task: 'Clean the Apartment',
-		id: 1528817077286,
-		completed: false
-	},
-	{
-		task: 'Get Groceries',
-		id: 1528817084358,
-		completed: false
-	},
-	{
-		task: 'Wash Car',
-		id: 1528817084359,
-		completed: false
-	},
-	{
-		task: 'Update Resume',
-		id: 1528817084360,
-		completed: false
+		task: '',
+		id: 0,
+		completed: ''
 	}
 ];
 
